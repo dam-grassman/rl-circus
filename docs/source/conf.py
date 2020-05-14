c77170210cf6afcd5e0a655d9dd6b7689ad36df4
dam-grassman <damien-grasset@hotmail.fr>
2020-05-14 10:27:37 -0400

    modif conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages', 
     'recommonmark', 
-    'sphinx_rtd_theme', 'nbsphinx']
+    'sphinx_rtd_theme']
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 
