3140f2775d346a5386ad2f642df9a4b433bd477c
dam-grassman <damien-grasset@hotmail.fr>
2020-05-14 12:02:09 -0400

    index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-master_doc = 'contents'
+master_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
